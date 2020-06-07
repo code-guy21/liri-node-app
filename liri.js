230e3abcd7f44d96f39ecd3c2cfad18936753871
Alexis San Javier <alexissj16@knights.ucf.edu>
2020-06-07 10:33:06 -0400

    completed fetchConcerts and renderConcerts functions to fetch and parse data from BandsInTown API

--- liri.js
+++ liri.js
@@ -1,22 +1,63 @@
+const axios = require("axios");
+const moment = require("moment");
 const keys = require("./keys.js");
 const Spotify = require("node-spotify-api");
-const axios = require("axios");
 
 let spotify = new Spotify(keys.spotify);
 
-switch (process.argv[2]) {
+let command = process.argv[2];
+let query = process.argv.slice(3).join("+");
+
+switch (command) {
   case "concert-this":
-    console.log("concert this");
+    fetchConcerts();
     break;
   case "spotify-this-song":
-    console.log("spotify");
+    fetchSong();
     break;
   case "movie-this":
-    console.log("movie this");
+    fetchMovie();
     break;
   case "do-what-it-says":
-    console.log("do what it says");
+    doWhatItSays();
     break;
   default:
     console.log("invalid command");
+}
+
+function fetchConcerts() {
+  axios
+    .get(
+      "https://rest.bandsintown.com/artists/" +
+        query +
+        "/events?app_id=codingbootcamp"
+    )
+    .then((resp) => {
+      renderConcerts(resp.data);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+}
+
+function renderConcerts(concerts) {
+  concerts.forEach((concert) => {
+    console.log("Venue: " + concert.venue.name);
+    console.log("Location: " + concert.venue.location);
+    console.log(
+      "Date: " + moment(concert.datetime).format("MM/DD/YYYY") + "\n"
+    );
+  });
+}
+
+function fetchSong() {
+  console.log("fetch song");
+}
+
+function fetchMovie() {
+  console.log("fetch movie");
+}
+
+function doWhatItSays() {
+  console.log("do what it says");
 }
