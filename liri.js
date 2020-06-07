d72dcc376941f455676b71b50fc0f39ab0f3bd19
Alexis San Javier <alexis@macbook-pro.lan>
2020-06-07 09:46:44 -0400

    added switch statement to handle liri commands

--- liri.js
+++ liri.js
@@ -1,2 +1,22 @@
-var keys = require("./keys.js");
-var Spotify = require("node-spotify-api");
+const keys = require("./keys.js");
+const Spotify = require("node-spotify-api");
+const axios = require("axios");
+
+let spotify = new Spotify(keys.spotify);
+
+switch (process.argv[2]) {
+  case "concert-this":
+    console.log("concert this");
+    break;
+  case "spotify-this-song":
+    console.log("spotify");
+    break;
+  case "movie-this":
+    console.log("movie this");
+    break;
+  case "do-what-it-says":
+    console.log("do what it says");
+    break;
+  default:
+    console.log("invalid command");
+}
